e1f4cc06c7f388439e038100f59d77f75e7c0168
Mohammed Alhammaly <m7md.al7male@gmail.com>
2026-05-02 23:29:26 +0300

    adjusted the player health and made the enemies start spawning when u click the button

--- SpoilzApplicationProject/Assets/Scripts/CubeInteraction.cs
+++ SpoilzApplicationProject/Assets/Scripts/CubeInteraction.cs
@@ -6,9 +6,12 @@ public class CubeInteraction : MonoBehaviour, IInteractable
     [SerializeField] private GameObject door;
     [SerializeField] private GameObject gameTimerText;
 
+    [SerializeField] private GameObject spawner;
+
     public void Interact()
     {
         door.SetActive(false);
+        spawner.SetActive(true);
         gameTimerText.SetActive(true);
         FindFirstObjectByType<GameTimer>().StartTimer();
     }
